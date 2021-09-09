76116b2482ef2ba50eb6f274fc228254b3d265e3
Joe Swantek <joseph.swantek@northwestern.edu>
2021-09-09 17:36:14 -0500

    SAGE-1022: copy samples thermal-raw

---
+++ mobotix_sdk/eventstreamclient/plugin-client/thermal-raw/SinkAudio.cpp
@@ -0,0 +1,88 @@
+//           ///          //                                  C++ Cross Platform
+//          /////        ////
+//         /// XXX     XXX ///            ///////////   /////////     ///   ///
+//        ///    XXX XXX    ///         ///             ///    ///   ///  ///
+//       ///       XXX       ///         /////////     ///      //  //////
+//      ///      XXX XXX      ///               ///   ///    ///   ///  ///
+//     ////    XXX     XXX    ////    ////////////  //////////    ///   ///
+//    ////                     ////
+//   ////  M  O  B  O  T  I  X  ////////////////////////////////////////////////
+//  //// Security Vision Systems //////////////////////////////////////////////
+//
+//
+///////
+     //
+     //  MOBOTIX EventStream Client SDK 
+     //
+     //  This software is licensed under the BSD license,
+     //  http://www.opensource.org/licenses/bsd-license.php
+     //
+     //  Copyright (c) 2005 - 2016, MOBOTIX AG
+     //  All rights reserved.
+     //
+     //  Redistribution and use in source and binary forms, with or without
+     //  modification, are permitted provided that the following conditions are
+     //  met:
+     //
+     //  - Redistributions of source code must retain the above copyright
+     //    notice, this list of conditions and the following disclaimer.
+     //
+     //  - Redistributions in binary form must reproduce the above copyright
+     //    notice, this list of conditions and the following disclaimer in the
+     //    documentation and/or other materials provided with the distribution.
+     //
+     //  - Neither the name of MOBOTIX AG nor the names of its contributors may
+     //    be used to endorse or promote products derived from this software
+     //    without specific prior written permission.
+     //
+     //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+     //  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+     //  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+     //  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+     //  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+     //  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+     //  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+     //  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+     //  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+     //  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+     //  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+     //
+///////
+
+#include "SinkAudio.h"
+
+#if defined (_MSC_VER)
+#include <fcntl.h>
+#include <io.h>
+#endif
+
+#include <iostream>
+
+SinkAudio::SinkAudio ()
+{
+   std::cout << "Creating dummy SinkAudio" << std::endl;
+}
+
+SinkAudio::~SinkAudio ()
+{
+   std::cout << "Deleting dummy SinkAudio" << std::endl;
+
+}
+
+MxPEG_ReturnCode SinkAudio::closeAudioDevice() {
+   return er_Success;
+}
+
+MxPEG_ReturnCode SinkAudio::initAudioDevice() {
+   return er_Success;
+}
+
+MxPEG_ReturnCode SinkAudio::doConsumeAudio(MxPEG_Audio::unique_ptr_t buffer) {
+   //do nothing, just ignore audio data in this example
+   return er_Success;
+}
+
+MxPEG_AudioFormat * SinkAudio::doGetTargetFormat() {
+   return nullptr;
+}
+
