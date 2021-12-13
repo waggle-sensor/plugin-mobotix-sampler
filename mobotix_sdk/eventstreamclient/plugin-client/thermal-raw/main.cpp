fdd1850e9ae7a873ead3957a66b69450378fa001
Joe Swantek <joseph.swantek@northwestern.edu>
2021-12-13 10:17:46 -0600

    SAGE-1179: auto-linting

--- mobotix_sdk/eventstreamclient/plugin-client/thermal-raw/main.cpp
+++ mobotix_sdk/eventstreamclient/plugin-client/thermal-raw/main.cpp
@@ -11,42 +11,42 @@
 //
 //
 ///////
-     //
-     //  MOBOTIX EventStream Client SDK 
-     //
-     //  This software is licensed under the BSD license,
-     //  http://www.opensource.org/licenses/bsd-license.php
-     //
-     //  Copyright (c) 2005 - 2016, MOBOTIX AG
-     //  All rights reserved.
-     //
-     //  Redistribution and use in source and binary forms, with or without
-     //  modification, are permitted provided that the following conditions are
-     //  met:
-     //
-     //  - Redistributions of source code must retain the above copyright
-     //    notice, this list of conditions and the following disclaimer.
-     //
-     //  - Redistributions in binary form must reproduce the above copyright
-     //    notice, this list of conditions and the following disclaimer in the
-     //    documentation and/or other materials provided with the distribution.
-     //
-     //  - Neither the name of MOBOTIX AG nor the names of its contributors may
-     //    be used to endorse or promote products derived from this software
-     //    without specific prior written permission.
-     //
-     //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-     //  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-     //  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-     //  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-     //  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-     //  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-     //  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-     //  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-     //  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-     //  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-     //  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-     //
+//
+//  MOBOTIX EventStream Client SDK
+//
+//  This software is licensed under the BSD license,
+//  http://www.opensource.org/licenses/bsd-license.php
+//
+//  Copyright (c) 2005 - 2016, MOBOTIX AG
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//  - Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+//  - Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+//  - Neither the name of MOBOTIX AG nor the names of its contributors may
+//    be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 ///////
 
 /*
@@ -85,27 +85,30 @@
 
 using namespace ie::MxPEG;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
    std::cout << "EventStream client SDK thermal raw data example" << std::endl;
 
-   if(argc < 2) {
-	   std::cout << "Missing parameter hostname/IP" << std::endl;
-	   std::cout << "Start with " << argv[0] << " [hostname|IP]" << std::endl;
-	   std::cout << "Press enter to exit" << std::endl;
-	   getchar();
-	   exit(1);
+   if (argc < 2)
+   {
+      std::cout << "Missing parameter hostname/IP" << std::endl;
+      std::cout << "Start with " << argv[0] << " [hostname|IP]" << std::endl;
+      std::cout << "Press enter to exit" << std::endl;
+      getchar();
+      exit(1);
    }
 
    char url[URL_SZ] = "";
-   snprintf(url,URL_SZ,"http://%s/control/eventstream.jpg",argv[1]);
+   snprintf(url, URL_SZ, "http://%s/control/eventstream.jpg", argv[1]);
 
 #ifdef _MSC_VER
    WSADATA wsaData;
    int iResult;
    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-   if (iResult != NO_ERROR) {
-	   std::cout << "WSAStartup failed: " << iResult << std::endl;
-	   return 1;
+   if (iResult != NO_ERROR)
+   {
+      std::cout << "WSAStartup failed: " << iResult << std::endl;
+      return 1;
    }
 #endif
 
@@ -132,7 +135,7 @@ int main(int argc, char** argv) {
    /*
     * Create a new eventstream client SDK object
     */
-   MxPEG_SDK::shared_ptr_t client = MxPEG_SDK_Factory::create(sinkAudio, sinkVideo, MxPEG_ImageMode::im_RGB, authHandler,notificationListener);
+   MxPEG_SDK::shared_ptr_t client = MxPEG_SDK_Factory::create(sinkAudio, sinkVideo, MxPEG_ImageMode::im_RGB, authHandler, notificationListener);
 
    /*
     * configure the client to connect to camera with hostname "i25"
@@ -169,7 +172,8 @@ int main(int argc, char** argv) {
     * The SDK library is single threaded, the function loop() acts as the main loop of the library. Call it repeatedly
     * from the applications main loop.
     */
-   while(true) {
+   while (true)
+   {
       client->loop();
    }
 
